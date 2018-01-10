aa06c537d9ba95204c020fdcabc1f622587eea62
Onselius <lars.onselius@telia.com>
2018-01-10 22:14:59 +0100

    Creating empty rooms for starting position and exit Also making sure that the starting position is not the exit

--- model/DungeonMap.py
+++ model/DungeonMap.py
@@ -12,11 +12,24 @@ class DungeonMap:
     def generate_rooms(self):
         for i in range(self.size):
             for j in range(self.size):
-                self.list_of_rooms.append(Room(i, j))
+                room = Room(i, j)
+                # If the position is the same as the starting position, set no monsters and no treasure
+                if i == self.playerPosX and j == self.playerPosY:
+                    room.list_of_monsters = []
+                    room.list_of_treasure = []
+                self.list_of_rooms.append(room)
 
     def generate_exit(self):
-        int_index = random.randrange(0, len(self.list_of_rooms))
-        self.list_of_rooms[int_index].is_exit = True
+        # Generate a random index in list range. Set exit to true, no monsters and no treasure
+        while True:
+            int_index = random.randrange(0, len(self.list_of_rooms))
+            room = self.list_of_rooms[int_index]
+            if not room.get_position == (self.playerPosX, self.playerPosY):
+                break
+
+        room.is_exit = True
+        room.list_of_monsters = []
+        room.list_of_treasure = []
 
     def generate_starting_pos(self, position):
         if position is "NW":
@@ -31,3 +44,4 @@ class DungeonMap:
         elif position is "SE":
             self.playerPosX = self.size
             self.playerPosY = self.size
+
