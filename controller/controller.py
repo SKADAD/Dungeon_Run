4cc4a5a693bf684f1d14f904804fcb94e6ccce29
validata <XKk7Ni2KcqYeri7L7p>
2018-01-12 21:46:35 +0100

    Code revised and some errors and bugs fixed Added more comments and some todos

--- controller/controller.py
+++ controller/controller.py
@@ -1,128 +1,151 @@
 import model.DungeonMap
-#import os
+# import os
+
 
 class AccountManager:
+    # This global variable remembers the users selected number if choice_validate confirms True:
+    global validated_user_input
 
     def __init__(self):
-        #self.character()
-        self.character_new_name = "Name"
-        self.character_load_name = "Name"
-        self.select_starting_pos = "NW"
-        self.select_char_type_new = "Class"
-        self.select_size_of_map = 5
+        # self.character() # TODO
+        self.character_hero = "Hero Type"
+        self.character_name = "Name"
+        self.starting_pos = "NW"
+        self.size_of_map = 1
+        self.running = True
 
-    # Wait for first input and print first menu:
-    def start(self):
-        self.choice_validator(input("Enter choice:\n 1. New Character \n 2. Existing Character\n"))
+    # First game menu and choices, validates input and finally calls for the next function:
+    def start_menu(self):
+        global user_input_number
+        max_value_for_menu = 2
+        clear_cmd()
+        print("Enter choice:\n 1. New Character \n 2. Existing Character\n")
+        choice = choice_validate_int(max_value_for_menu)
+        if not choice:
+            self.start_menu()
+        elif choice:
+            if user_input_number == 1:
+                self.menu_char_new()
+            elif user_input_number == 2:
+                self.menu_char_existing()
 
-    # Try to validate user input and if OK send it to start_main_menu
-    def choice_validator(self, choice):
-        self.clear_cmd()
-        try:
-            choice_input = int(choice)
-            if not (1 <= choice_input <= 2):
-                raise ValueError()
-            else:
-                self.start_main_menu(choice_input)
-                return
-        except ValueError:
-            print("Not a valid choice. You must enter 1 or 2 to continue.")
-            self.start()
-
-    # Switch / Options in first menu:
-    def start_main_menu(self, choice):
-        switcher = {
-            1: self.menu_char_new,
-            2: self.menu_char_existing,
-        }
-        func = switcher.get(choice)
-        return func()
-
-    # Let the user create a new char:
+    # Create new character intro and finally calls new player name
     def menu_char_new(self):
-        choice = int(input("Enter character number:\n1. Warrior\n2. Wizard\n3. Thief\n"))
-        try:
-            if not (1 <= choice <= 3):
-                raise ValueError()
+        clear_cmd()
+        max_value_of_menu = 3
+        print("Choose hero by number:\n1. Warrior\n2. Wizard\n3. Thief\n")
+        choice = choice_validate_int(max_value_of_menu)
+        if not choice:
+            print("Invalid choice")
+            self.menu_char_new()
+        elif choice:
+            if user_input_number == 1:
+                self.character_hero = "Warrior"
+            elif user_input_number == 2:
+                self.character_hero = "Wizard"
+            elif user_input_number == 3:
+                self.character_hero = "Thief"
             else:
-                if choice == 1:
-                    self.select_char_type_new = "Warrior"
-                elif choice == 2:
-                    self.select_char_type_new = "Wizard"
-                elif choice == 3:
-                    self.select_char_type_new = "Thief"
-                self.clear_cmd()
-                self.character_new_name = input("Enter your characters name:\n")
-# TODO1 check if user name already exists
-# TODO2 save character after created
-                controller.s
-                s.load_character(TheUser)
-                self.menu_map_size()
-                return
-        except ValueError:
-            print("That isn't a valid choice.\nYou must enter a number from 0 to 4 to continue.")
+                print("Unexpected.")
+            # Sends the user to next menu
+            self.menu_new_player_name()
+        else:
+            print("Unexpected..")
 
-    # User chooses map size before starting:
+    # Create new name for new character and starts map:
+    def menu_new_player_name(self):
+        choice = str(input("Enter a character name:\n"))
+        # TODO check if user name already exists
+        # TODO save character to account
+        self.character_name = choice
+        print("Your new name is: " + self.character_name)
+        self.menu_map_size()
+
+    # User selects map size and set position function starts:
     def menu_map_size(self):
-        self.clear_cmd()
-        choice = int(input("Select size of map:\n1. Small\n2. Not too small and not too big\n3. Large\n"))
-        try:
-            if not(1 <= choice <= 3):
-                raise ValueError()
-            if choice == 1:
-                self.select_size_of_map_input = 4
-            elif choice == 2:
-                self.select_size_of_map_input = 5
-            elif choice == 3:
-                self.select_size_of_map_input = 8
-            self.menu_player_position()
-        except ValueError:
-            print("Not a valid number")
+        clear_cmd()
+        max_value_of_menu = 3
+        print("Select map size:\n1. 16 rooms\n2. 25 rooms \n3. 64 rooms\n")
+        choice = choice_validate_int(max_value_of_menu)
+        if not choice:
+            clear_cmd()
             self.menu_map_size()
-# TODO3 Request map to be created etc with stored variables
+        elif choice:
+            if user_input_number == 1:
+                print("Choice =4")
+                self.size_of_map = 4
+            elif user_input_number == 2:
+                self.size_of_map = 5
+            elif user_input_number == 3:
+                print("Choice =8")
+                self.size_of_map = 8
+            self.menu_player_position()
 
-
+    # Position is selected. The end is todo:
     def menu_player_position(self):
-        self.clear_cmd()
-        select_starting_pos_input = int(input("Enter starting position:\n1. North West\n2. North East\n3. South West\n4. South East\n"))
-        try:
-            if select_starting_pos_input == 1:
-                self.select_starting_pos = "NW"
-                print("NW")
-            elif select_starting_pos_input == 2:
-                self.select_starting_pos = "NE"
-                print("NE")
-            elif select_starting_pos_input == 3:
-                self.select_starting_pos = "SW"
-                print("SW")
-            elif select_starting_pos_input == 4:
-                self.select_starting_pos = "SE"
-                print("SE")
+        clear_cmd()
+        max_value_of_menu = 4
+        print("Enter starting position:\n1. North West\n2. North East\n3. South West\n4. South East\n")
+        choice = choice_validate_int(max_value_of_menu)
+        if not choice:
+            self.menu_player_position()
+        elif choice:
+            if user_input_number == 1:
+                self.starting_pos = "NW"
+            elif user_input_number == 2:
+                self.starting_pos = "NE"
+            elif user_input_number == 3:
+                self.starting_pos = "SW"
+            elif user_input_number == 4:
+                self.starting_pos = "SE"
             else:
-                print("Not a valid input")
-        except:
-            print("Wrong input")
+                print("Unexpected, selecting default:" + self.starting_pos)
+            self.present_result()
+        # TODO send character data from here
+        else:
+            print("Unexpected2")
 
-        print(self.character_new_name)
-        print(self.select_char_type_new)
-        print(self.select_size_of_map)
-        print(self.select_starting_pos)
+    # Prints the choices made in menu:
+    def present_result(self):
+        print("Name: " + self.character_name)
+        print("Hero Class: " + self.character_hero)
+        sizeofmap = str(self.size_of_map * self.size_of_map)
+        print("Map Total Rooms: " + sizeofmap)
+        print("Map starting position: " + self.starting_pos)
 
     # Load existing character
     def menu_char_existing(self):
-# TODO Look if char exists and if so load:
-
-        self.clear_cmd()
+        # TODO Look if char exists and if so load:
+        clear_cmd()
         print("Choice 2, trying to load existing char..")
 
-    # Clear CLI / GUI from lines when needed:
-    def clear_cmd(self):
-        try:
-            print("\n\n\n\n\n\n\n\n\n\n\n")
-            #os.system('CLR')
-            #os.system('clear')
-        except:
-            print("Tried to clear CLI but failed")
+
+# Clear CLI / GUI from lines when needed:
+def clear_cmd():
+    try:
+        print("\n\n\n\n")
+    # disabled (debug mode on)
+    # os.system('CLR')
+    # os.system('clear')
+    except:
+        print("Tried to clear CLI but failed")
+
+
+# Validates the users input when called
+def choice_validate_int(highest_value_in_menu):
+    global user_input_number
+    try:
+        user_choice = int(input(""))
+        # Menu choices that can be selected is from 1 up to or as <number defined in the call to this function>
+        if not (1 <= user_choice <= highest_value_in_menu):
+            raise ValueError()
+        else:
+            # When True remembers the choice (int)
+            user_input_number = user_choice
+            return True
+    except ValueError:
+        return False
+
 
 start = AccountManager()
-start.start()
+start.start_menu()
