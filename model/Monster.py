303fd27f1ab192f9434fba4b7017e082198c9405
kramdolf407 <pfpeterflodmark@gmail.com>
2018-01-12 15:13:43 +0100

    skapat toString på Monster

--- model/Monster.py
+++ model/Monster.py
@@ -35,3 +35,10 @@ class Monster:
             self.toughness = 4
             self.attack = 7
             self.agility = 2
+
+    def toString(self):
+        print("Monster type: " + self.monster_type + " Initiative: "+ str(self.initiative) +
+              " toughness: "+ str(self.toughness) + " attack: "+ str(self.attack)+ " agility: "+ str(self.agility))
+
+troll = Monster("Troll")
+print(troll.toString())
