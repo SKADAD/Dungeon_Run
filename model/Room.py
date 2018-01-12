5fc6408cc17f3a57bfe1bdf547a500bdae98ee33
kramdolf407 <pfpeterflodmark@gmail.com>
2018-01-12 15:04:18 +0100

    gjort generate monster & treasure på varje rum (klart)

--- model/Room.py
+++ model/Room.py
@@ -1,5 +1,5 @@
 import random
-import model.Monster
+from model.Monster import Monster
 
 class Room:
 
@@ -19,22 +19,22 @@ class Room:
         list_of_monsters = []
         value1 = random.randrange(0, 4)
         if value1 == 0:
-            spider = model.Monster("Giant_Spider")
+            spider = Monster("Giant_Spider")
             list_of_monsters.append(spider)
 
         value2 = random.randrange(0, 99)
         if value2 < 15:
-            skeleton = model.Monster("Skeleton")
+            skeleton = Monster("Skeleton")
             list_of_monsters.append(skeleton)
 
         value3 = random.randrange(0, 9)
         if value3 == 0:
-            orch = model.Monster("Orch")
+            orch = Monster("Orch")
             list_of_monsters.append(orch)
 
         value4 = random.randrange(0, 19)
         if value4 == 0:
-            troll = model.Monster("Troll")
+            troll = Monster("Troll")
             list_of_monsters.append(troll)
 
         return list_of_monsters
