34a2b16eca345bbb24353be1a2fbe765de936f9c
SKADAD <32932385+SKADAD@users.noreply.github.com>
2018-01-15 10:27:42 +0100

    fix

--- controller/controller.py
+++ controller/controller.py
@@ -1,8 +1,7 @@
-import os
 from model.DungeonMap import *
 
 class Controller:
-
+    dungeonMap = DungeonMap
 
     def toPrint(string_to_print):
         os.system('cls')
@@ -10,8 +9,7 @@ class Controller:
 
 
     def player_movement(direction):
-        if direction.lower == "w" or "a" or "s" or "d":
-            dungeonMap.move_player(direction)
+        if direction == "w" or "a" or "s" or "d":
+            DungeonMap.move_player(direction)
         else:
             print("fool, wrong step")
-            Controller.toPrint(DungeonMap.print_map())
