51ab96d4f2d13c8879637afe0566523e0d36dd8d
kramdolf407 <pfpeterflodmark@gmail.com>
2018-01-15 13:52:50 +0100

    added methods for player_attack and monster_attack in combat controller

--- controller/CombatController.py
+++ controller/CombatController.py
@@ -1,6 +1,5 @@
 import random
 
-
 class CombatController:
 
     def __init__(self, controller, list_of_monsters):
@@ -8,6 +7,7 @@ class CombatController:
         self.player = controller.player
         self.order_of_attack = []
         self.create_order_of_attack(list_of_monsters)
+
 
     def create_order_of_attack(self, list_of_monsters):
         dict_of_initiative = {}
@@ -26,3 +26,19 @@ class CombatController:
         for i in range(number_of_dices):
             value += random.randint(1, 6)
         return value
+
+    def player_attack(self, monster_target):
+        player_attack = self.roll_dice(self.player.attack)
+        enemy_agility = self.roll_dice(monster_target.agility)
+        if player_attack > enemy_agility:
+            monster_target.durability -= 1
+            if monster_target.durability <= 0:
+                print("Enemy down")
+
+    def monster_attack(self, monster):
+        monster_attack = self.roll_dice(monster.attack)
+        player_agility = self.roll_dice(self.player.agility)
+        if monster_attack > player_agility:
+            self.player.durability -= 1
+            if self.player.durability <= 0:
+                print("game over")
--- model/Monster.py
+++ model/Monster.py
@@ -4,38 +4,38 @@ class Monster:
     def __init__(self, monster_type):
         self.monster_type = monster_type
         self.initiative = 0
-        self.toughness = 0
+        self.durability = 0
         self.attack = 0
         self.agility = 0
 
         if monster_type == "Giant_Spider":
             self.monster_type = monster_type
             self.initiative = 7
-            self.toughness = 1
+            self.durability = 1
             self.attack = 2
             self.agility = 3
 
         if monster_type == "Skeleton":
             self.monster_type = monster_type
             self.initiative = 4
-            self.toughness = 2
+            self.durability = 2
             self.attack = 3
             self.agility = 3
 
         if monster_type == "Orch":
             self.monster_type = monster_type
             self.initiative = 6
-            self.toughness = 3
+            self.durability = 3
             self.attack = 4
             self.agility = 4
 
         if monster_type == "Troll":
             self.monster_type = monster_type
             self.initiative = 2
-            self.toughness = 4
+            self.durability = 4
             self.attack = 7
             self.agility = 2
 
     def toString(self):
         print("Monster type: " + self.monster_type + " Initiative: "+ str(self.initiative) +
-              " toughness: "+ str(self.toughness) + " attack: "+ str(self.attack)+ " agility: "+ str(self.agility))
+              " toughness: "+ str(self.durability) + " attack: "+ str(self.attack)+ " agility: "+ str(self.agility))
