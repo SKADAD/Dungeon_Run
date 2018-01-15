f85e9188036f09716010bb5eca25f7ac117dc8c9
SKADAD <32932385+SKADAD@users.noreply.github.com>
2018-01-15 09:30:03 +0100

    Push

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -1,16 +1,22 @@
 import os
 from model.DungeonMap import *
 
+class Controller:
 
-dungeonMap = DungeonMap()
-
-def toPrint(string_to_print):
-    os.system('cls')
-    print(DungeonMap.string_to_print)
+    def __init__(self):
 
 
-def player_movement(direction):
-    if direction.lower == "w" or "a" or "s" or "d":
-        dungeonMap.move_player(direction)
-    else:
-        print("fool, wrong step")
+    dungeonMap = DungeonMap()
+
+    def toPrint(string_to_print):
+        os.system('cls')
+        print(string_to_print)
+
+
+    def player_movement(direction):
+        if direction.lower == "w" or "a" or "s" or "d":
+            dungeonMap.move_player(direction)
+        else:
+            print("fool, wrong step")
+
+    toPrint(dungeonMap.print_map())
