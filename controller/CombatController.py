ef322d2558a8a93bd5ae81125ddf498418c6dacb
kramdolf407 <pfpeterflodmark@gmail.com>
2018-01-16 17:01:27 +0100

    added player flee

--- controller/CombatController.py
+++ controller/CombatController.py
@@ -90,8 +90,8 @@ class CombatController:
         flee_var = self.player.agility * 10
         dice_roll = random.randrange(0, 100)
         if dice_roll <= flee_var:
-            self.controller.dungeon_map.playerposX = self.controller.dungeon_map.last_position[0]
-            self.controller.dungeon_map.playerposY = self.controller.dungeon_map.last_position[1]
+            self.controller.dungeon_map.playerPosX = self.controller.dungeon_map.last_position[0]
+            self.controller.dungeon_map.playerPosY = self.controller.dungeon_map.last_position[1]
             return True
         else:
             return False
